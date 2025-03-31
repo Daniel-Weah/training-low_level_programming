e5a491e2d885ff2919ea41622adae15a4ca772a8
Mr. Software <weahjrdanielk@gmail.com>
2025-03-31 14:01:11 +0000

    Write a function that multiply two integer and also updated the main.h file by add the prototype of this function into it

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two integers
+ *@a: holds the value of the first integer
+ *@b: holds the value of the second integer
+ *Return: return the product
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
